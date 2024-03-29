727b0558d29536b85da5afe1a400248987dde4fb
Jazastry <jazastry@gmail.com>
2024-03-29 12:01:35 +0200

    updates a function name

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,39 +1,39 @@
-import { open, writeFile } from 'node:fs/promises'
-import { resolve } from 'node:path'
-import { Stream } from 'node:stream'
-import { restoreCache, saveCache } from '@actions/cache'
-import { RemoteCacheImplementation } from 'nx-remotecache-custom'
+import {open, writeFile} from 'node:fs/promises'
+import {resolve} from 'node:path'
+import {Stream} from 'node:stream'
+import {restoreCache, saveCache} from '@actions/cache'
+import {RemoteCacheImplementation} from 'nx-remotecache-custom'
 
 const PREFIX = 'nx'
 
 const prefix = (filename: string) => `${PREFIX}-${filename}`
 
-function streamToString(stream: Stream): Promise<Buffer> {
-    const chunks = []
-    return new Promise((resolve, reject) => {
-        stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)))
-        stream.on('error', (err) => reject(err))
-        stream.on('end', () => resolve(Buffer.concat(chunks)))
-    })
+function streamToBuffer(stream: Stream): Promise<Buffer> {
+  const chunks = []
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)))
+    stream.on('error', (err) => reject(err))
+    stream.on('end', () => resolve(Buffer.concat(chunks)))
+  })
 }
 
 export const createGithubActionsCache = (
-    path: string
+  path: string
 ): RemoteCacheImplementation => ({
-    name: 'Github Actions Cache',
+  name: 'Github Actions Cache',
 
-    fileExists: (filename: string) =>
-        restoreCache([path], prefix(filename)).then(Boolean),
+  fileExists: (filename: string) =>
+    restoreCache([path], prefix(filename)).then(Boolean),
 
-    retrieveFile: async (filename: string) =>
-        open(resolve(path, prefix(filename))).then((fd) =>
-            fd.createReadStream()
-        ),
+  retrieveFile: async (filename: string) =>
+    open(resolve(path, prefix(filename))).then((fd) =>
+      fd.createReadStream()
+    ),
 
-    storeFile: async (filename: string, data: Stream) => {
-        const d = await streamToString(data)
-        writeFile(resolve(path, prefix(filename)), d).then(() =>
-            saveCache([path], prefix(filename))
-        )
-    },
+  storeFile: async (filename: string, data: Stream) => {
+    const d = await streamToBuffer(data)
+    writeFile(resolve(path, prefix(filename)), d).then(() =>
+      saveCache([path], prefix(filename))
+    )
+  },
 })
